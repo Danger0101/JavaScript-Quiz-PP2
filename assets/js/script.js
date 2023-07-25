09a092f8fb5a2c02b61fda1e8ef1b2307305f794
John Hummel <34178990+Danger0101@users.noreply.github.com>
2023-07-25 17:48:43 +0100

    Updated choices for each loop
    Updated choices for each loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,6 +58,25 @@ function getNewQuestion() {
 }
 
 choices.forEach(choice => {
+        choice.addEventListener('click', e => {
+                if (!acceptingAnswers) return;
+
+                acceptingAnswers = false;
+                const selectedChoice = e.target;
+                const selectedAnswer = selectedChoice.dataset['number'];
+
+                const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+                if (classToApply === 'correct'){
+                        incrementScore(correctBonus);
+                }
+                selectedChoice.parentElement.classList.add(classToApply);
+
+                setTimeout(() => {
+                        selectedChoice.parentElement.classList.remove(classToApply);
+                        getNewQuestion();
+                }, 1000);
+        });
 });
 
 function incrementScore(num) {
