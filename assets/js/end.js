a5da55fa3d9ca1bbbdeefa044324e30323707b62
John Hummel <34178990+Danger0101@users.noreply.github.com>
2023-08-12 16:59:39 +0100

    Added rotation check and display script
    Added rotation check and display script

--- assets/js/end.js
+++ assets/js/end.js
@@ -29,3 +29,32 @@ saveHighScore = (e) => {
         localStorage.setItem('highScores', JSON.stringify(highScores));
         window.location.assign('index.html');
 };
+/**
+ * Function checks Orientation of device and shows relevant portions depnding
+ * on orientation and screen resolution
+ */
+function checkOrientation() {
+        const mainContainer = document.getElementById('main');
+        const quiz = document.getElementById('quiz');
+        const rotateMessage = document.getElementById('rotate-message');
+        if (window.matchMedia("(max-width: 768px)").matches) {
+                // Only activate on mobile devices (width <= 768px)
+                if (window.innerWidth > window.innerHeight) {
+                        // Landscape
+                        mainContainer.style.display = 'none';
+                        rotateMessage.style.display = 'flex';
+                } else {
+                        // Portrait
+                        mainContainer.style.display = 'flex';
+                        rotateMessage.style.display = 'none';
+                }
+        } else {
+                // Reset styles for desktop
+                mainContainer.style.display = 'flex';
+                rotateMessage.style.display = 'none';
+        }
+}
+checkOrientation();
+// Event listenrs looking for a change in screen orientation
+window.addEventListener('orientationchange', checkOrientation);
+window.addEventListener('resize', checkOrientation);
--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -12,3 +12,32 @@ highScoresList.innerHTML = highScores
  * converts map into a single string allowing it to display multiple users with out sepperation
  */
 .join("");
+/**
+ * Function checks Orientation of device and shows relevant portions depnding
+ * on orientation and screen resolution
+ */
+function checkOrientation() {
+        const mainContainer = document.getElementById('main');
+        const quiz = document.getElementById('quiz');
+        const rotateMessage = document.getElementById('rotate-message');
+        if (window.matchMedia("(max-width: 768px)").matches) {
+                // Only activate on mobile devices (width <= 768px)
+                if (window.innerWidth > window.innerHeight) {
+                        // Landscape
+                        mainContainer.style.display = 'none';
+                        rotateMessage.style.display = 'flex';
+                } else {
+                        // Portrait
+                        mainContainer.style.display = 'flex';
+                        rotateMessage.style.display = 'none';
+                }
+        } else {
+                // Reset styles for desktop
+                mainContainer.style.display = 'flex';
+                rotateMessage.style.display = 'none';
+        }
+}
+checkOrientation();
+// Event listenrs looking for a change in screen orientation
+window.addEventListener('orientationchange', checkOrientation);
+window.addEventListener('resize', checkOrientation);
