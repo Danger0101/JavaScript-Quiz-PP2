b9780ace42a20b77382958141cfe3f2fee5a600a
John Hummel <34178990+Danger0101@users.noreply.github.com>
2023-08-17 16:00:26 +0100

    Add index.js
    Moved the script for index page into a JavaScript file.

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+const howToPlayBtn = document.getElementById("how-to-play-btn");
+const howToPlayDiv = document.getElementById("how-to-play-div");
+// Toggles visibility of how to play div
+howToPlayBtn.addEventListener('click', () => {
+        howToPlayDiv.classList.toggle('hidden');
+});
+// Checks if screen width mas is 768px or lower and the orientation based on height of the device
+function checkOrientation() {
+        const mainContainer = document.getElementById('main');
+        const quiz = document.getElementById('quiz');
+        const rotateMessage = document.getElementById('rotate-message');
+        if (window.matchMedia("(max-width: 768px)").matches) {
+                // Only activate on mobile devices (width <= 768px)
+                if (window.innerWidth > window.innerHeight) {
+                        // Landscape
+                        mainContainer.style.display = 'none';
+                        rotateMessage.style.display = 'flex';
+                } else {
+                        // Portrait
+                        mainContainer.style.display = 'flex';
+                        rotateMessage.style.display = 'none';
+                }
+        } else {
+                // Reset styles for desktop
+                mainContainer.style.display = 'flex';
+                rotateMessage.style.display = 'none';
+        }
+}
+checkOrientation();
+// Event listenrs looking for a change in screen orientation
+window.addEventListener('orientationchange', checkOrientation);
+window.addEventListener('resize', checkOrientation);
